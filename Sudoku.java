fa3a08deedc5a1321c664a25bed75bd96e0536b6
Francisco Mateus <ft.mateus@campus.fct.unl.pt>
2024-08-14 18:05:54 +0000

    Multiple games multithreading support

--- Sudoku.java
+++ Sudoku.java
@@ -6,9 +6,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Sudoku {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         InputStream input = System.in;
 
@@ -18,17 +21,33 @@ public class Sudoku {
             System.out.println("Parsing input from stdin");
 
         var boards = parseSudokuFile(input);
+        
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+        var results = new LinkedList<SudokuGame>();
 
         for(var board : boards)
         {
-            System.out.println();
-            
             var s = new SudokuGame(true, false);
-            
-            s.solveSudoku(board);
-            
-            s.printBoardSimple();
 
+            executor.submit(() -> {
+                s.solveSudoku(board);
+            });
+
+            results.add(s);
+            
+            // s.printBoardSimple();
+        }
+
+        executor.shutdown();
+
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+        for(var r : results) {
+            System.out.println();
+
+            r.printBoardSimple();
+            
             System.out.println("#####################");
         }
     }
